29d6716b5d77e0b781cfeaee15830bf6a9da2184
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-22 21:20:56 +0000

    Add operate function

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 
+
 function add(num1, num2) {
     return num1 + num2;
-};
+}
 
 
 function sub(num1, num2) {
     return num1 - num2;
-};
+}
 
 
 function mul(num1, num2) {
     return num1 * num2;
-};
+}
 
 
 function div(num1, num2) {
     return num1 / num2;
+}
+
+
+const OPERATOR_FNS = {
+    add,
+    sub,
+    mul,
+    div,
 };
+
+
+function operate(op, num1, num2) {
+    return OPERATOR_FNS[op](num1, num2);
+}
 
