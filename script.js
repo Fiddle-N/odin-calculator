b5d43bf645d18815861460c186c62fdf752f728b
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-22 23:53:47 +0000

    Populate display when buttons are pressed

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const OPERATORS = {
+    ADD: '+',
+    SUB: '-',
+    MUL: '*',
+    DIV: '/',
+}
 
 
 function add(num1, num2) {
@@ -32,3 +38,47 @@ function operate(op, num1, num2) {
     return OPERATOR_FNS[op](num1, num2);
 }
 
+
+const calc = {
+    num1: 0,
+    num2: 0,
+    op: null,
+
+    _display: '',
+
+    get display() {
+        return this._display;
+    },
+
+    set display(txt) {
+        this._display = txt;
+        const display = document.querySelector('.display');
+        display.textContent = txt;
+    },
+
+    appendDisplay(digit) {
+        if (this.display === '0') {
+            this.display = digit;
+        }
+        else {
+            this.display += digit;
+        }
+    },
+
+    btnNum(event) {
+        const numStr = event.target.textContent;
+        this.appendDisplay(numStr);
+    } 
+
+}
+
+
+function main() {
+    const btnNums = document.querySelectorAll('.keys-digit');
+    for (const btnNum of btnNums) {
+        btnNum.addEventListener('click', (event) => calc.btnNum(event));
+    }
+}
+
+
+main();
