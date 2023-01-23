157606939b9e0692ffdaddb1db3da4b45463ccf6
Fadil Nohur <fadil.nohur@outlook.com>
2023-01-23 00:23:46 +0000

    Implement one calculation operation

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const OPERATORS = {
-    ADD: '+',
-    SUB: '-',
-    MUL: '*',
-    DIV: '/',
-}
 
 
 function add(num1, num2) {
@@ -27,10 +21,10 @@ function div(num1, num2) {
 
 
 const OPERATOR_FNS = {
-    add,
-    sub,
-    mul,
-    div,
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div,
 };
 
 
@@ -43,8 +37,12 @@ const calc = {
     num1: 0,
     num2: 0,
     op: null,
+    opJustPressed: false,
+
+    equals: false,
 
     _display: '',
+    _displayHistory: '',
 
     get display() {
         return this._display;
@@ -54,6 +52,16 @@ const calc = {
         this._display = txt;
         const display = document.querySelector('.display');
         display.textContent = txt;
+    },
+
+    get displayHistory() {
+        return this._displayHistory;
+    },
+
+    set displayHistory(txt) {
+        this._displayHistory = txt;
+        const displayHistory = document.querySelector('.displayHistory');
+        displayHistory.textContent = txt;
     },
 
     appendDisplay(digit) {
@@ -67,8 +75,28 @@ const calc = {
 
     btnNum(event) {
         const numStr = event.target.textContent;
+        if (this.opJustPressed) {
+            this.display = '';
+            this.opJustPressed = false;
+        }
         this.appendDisplay(numStr);
-    } 
+    },
+
+    btnOp(event) {
+        const op = event.target.dataset.op;
+        const opTxt = event.target.textContent;
+        this.op = op;
+        this.opJustPressed = true;
+        this.num1 = Number(this.display);
+        this.displayHistory = `${this.display} ${opTxt}`;
+    },
+
+    btnEquals() {
+        this.num2 = Number(this.display);
+        this.displayHistory = '';
+        const result = operate(this.op, this.num1, this.num2);
+        this.display = String(result);
+    }
 
 }
 
@@ -78,6 +106,14 @@ function main() {
     for (const btnNum of btnNums) {
         btnNum.addEventListener('click', (event) => calc.btnNum(event));
     }
+
+    const btnOps = document.querySelectorAll('.keys-operator');
+    for (const btnOp of btnOps) {
+        btnOp.addEventListener('click', (event) => calc.btnOp(event));
+    }
+
+    const btnEquals = document.querySelector('.keys-equals');
+    btnEquals.addEventListener('click', () => calc.btnEquals())
 }
 
 
